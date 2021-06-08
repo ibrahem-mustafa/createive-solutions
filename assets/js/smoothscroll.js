55dc1ec26ffe256abf0d89b6dca4275486986d1f
ibrahem mustafa <ib.aldomyate.bus@gmail.com>
2021-06-08 22:38:37 +0200

    fix scroll

--- assets/js/smoothscroll.js
+++ assets/js/smoothscroll.js
@@ -12,13 +12,24 @@ $(document).ready(function(){
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
+      const scrollToElement = $(hash)
+      console.log(scrollToElement)
+      if (!scrollToElement.length) {
+        window.location.hash = 'scroll';
+      } else {
+        $('html, body').animate({
+          scrollTop: scrollToElement.offset().top
+        }, 800, function(){
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          if (hash.startsWith('#img')) {
+            window.location.hash = hash;
+          } else {
+            window.location.hash = 'scroll';
+            
+          }
+        });
+      }
+     
     } // End if
   });
 });
